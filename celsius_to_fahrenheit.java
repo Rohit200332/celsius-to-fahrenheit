037366b5cf95fdab60ba8207b323ea4c8e311dc4
Debajyoti Bandyopadhyay <bandyopadhyaydebajyoti2003@gmail.com>
2023-10-30 10:08:46 +0530

    celsius to fahrenheit

---
+++ celsius_to_fahrenheit.java
@@ -0,0 +1,14 @@
+// Java Program to Convert Celsius into Fahrenheit 
+class celsius_to_fahrenheit { 
+	public static void main(String[] args) 
+	{ 
+		
+		double celsius = 10.0, fahrenheit = 0.0; 
+		
+		 
+		fahrenheit = (celsius * 1.8) + 32; 
+		System.out.println( 
+			"  Temperature in Fahrenheit:"
+			+ fahrenheit); 
+	} 
+}
